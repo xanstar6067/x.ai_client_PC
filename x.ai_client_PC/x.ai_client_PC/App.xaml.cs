b02276c8afc1ae4ff7ec639668ed139d3d2bec71
xanstar6067 <district6067@gmail.com>
2026-06-16 15:46:47 +0300

    2

---
+++ x.ai_client_PC/x.ai_client_PC/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace x.ai_client_PC
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
