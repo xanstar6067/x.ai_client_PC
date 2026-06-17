c51811760709e876a8033d93ac061b143bc53ed1
xanstar6067 <district6067@gmail.com>
2026-06-17 13:35:40 +0300

    Сброс проекта к начальному состоянию.

--- x.ai_client_PC/x.ai_client_PC/MainWindow.xaml.cs
+++ x.ai_client_PC/x.ai_client_PC/MainWindow.xaml.cs
@@ -1,80 +1,24 @@
+﻿using System.Text;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
-using x.ai_client_PC.Models;
-using x.ai_client_PC.ViewModels;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace x.ai_client_PC;
-
-public partial class MainWindow : Window
+namespace x.ai_client_PC
 {
-    public MainWindow(MainViewModel viewModel)
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-        DataContext = viewModel;
-        ApiKeyBox.PasswordChanged += (_, _) => viewModel.ApiKeyInput = ApiKeyBox.Password;
-        Loaded += async (_, _) =>
+        public MainWindow()
         {
-            try
-            {
-                await viewModel.InitializeAsync();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString(), "Initialization error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        };
-    }
-
-    private void InputBox_PreviewKeyDown(object sender, KeyEventArgs e)
-    {
-        if (e.Key == Key.Enter && Keyboard.Modifiers != ModifierKeys.Shift)
-        {
-            if (DataContext is MainViewModel vm && vm.SendCommand.CanExecute(null))
-            {
-                vm.SendCommand.Execute(null);
-                e.Handled = true;
-            }
-        }
-    }
-
-    private void Window_KeyDown(object sender, KeyEventArgs e)
-    {
-        if (e.Key == Key.F && Keyboard.Modifiers == ModifierKeys.Control)
-        {
-            // Focus handled via binding search; could focus search box here
-        }
-    }
-
-    private void Window_DragOver(object sender, DragEventArgs e)
-    {
-        if (e.Data.GetDataPresent(DataFormats.FileDrop))
-        {
-            e.Effects = DragDropEffects.Copy;
-        }
-        else
-        {
-            e.Effects = DragDropEffects.None;
-        }
-
-        e.Handled = true;
-    }
-
-    private async void Window_Drop(object sender, DragEventArgs e)
-    {
-        if (!e.Data.GetDataPresent(DataFormats.FileDrop) || DataContext is not MainViewModel vm)
-        {
-            return;
-        }
-
-        var files = (string[])e.Data.GetData(DataFormats.FileDrop)!;
-        await vm.AddDroppedFilesCommand.ExecuteAsync(files);
-    }
-
-    private async void ChatItem_DoubleClick(object sender, MouseButtonEventArgs e)
-    {
-        if (sender is FrameworkElement { DataContext: ChatSession chat } && DataContext is MainViewModel vm)
-        {
-            await vm.SelectChatCommand.ExecuteAsync(chat);
+            InitializeComponent();
         }
     }
 }
